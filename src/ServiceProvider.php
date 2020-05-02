869488ec7eb29b366d5418e0d4d22709bc0a020e
Dyan Galih <dyan.galih@gmail.com>
2020-05-02 19:34:36 +0700

    Fix service provider use class

--- src/ServiceProvider.php
+++ src/ServiceProvider.php
@@ -3,6 +3,27 @@
 namespace WebAppId\LazyCrud;
 
 use Illuminate\Support\ServiceProvider as BaseServiceProvider;
+use WebAppId\LazyCrud\Commands\ControllerDeleteMakeCommand;
+use WebAppId\LazyCrud\Commands\ControllerDetailMakeCommand;
+use WebAppId\LazyCrud\Commands\ControllerIndexMakeCommand;
+use WebAppId\LazyCrud\Commands\ControllerStoreMakeCommand;
+use WebAppId\LazyCrud\Commands\ControllerUpdateMakeCommand;
+use WebAppId\LazyCrud\Commands\CrudMakeCommand;
+use WebAppId\LazyCrud\Commands\ModelMakeCommand;
+use WebAppId\LazyCrud\Commands\RepositoryContractMakeCommand;
+use WebAppId\LazyCrud\Commands\RepositoryMakeCommand;
+use WebAppId\LazyCrud\Commands\RepositoryRequestMakeCommand;
+use WebAppId\LazyCrud\Commands\RequestMakeCommand;
+use WebAppId\LazyCrud\Commands\RequestSearchMakeCommand;
+use WebAppId\LazyCrud\Commands\RouteFormatMakeCommand;
+use WebAppId\LazyCrud\Commands\ServiceContractMakeCommand;
+use WebAppId\LazyCrud\Commands\ServiceMakeCommand;
+use WebAppId\LazyCrud\Commands\ServiceRequestMakeCommand;
+use WebAppId\LazyCrud\Commands\ServiceResponseListMakeCommand;
+use WebAppId\LazyCrud\Commands\ServiceResponseMakeCommand;
+use WebAppId\LazyCrud\Commands\TestRepositoryMakeCommand;
+use WebAppId\LazyCrud\Commands\TestServiceMakeCommand;
+use WebAppId\LazyCrud\Commands\TestSmartTestCaseMakeCommand;
 
 class ServiceProvider extends BaseServiceProvider
 {
